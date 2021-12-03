f0fda509552a4adbd98caed71b3d42e805352fff
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-12-03 23:31:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xkz3z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const port = 5000
+
+// console.log(process.env.DB_PASS)
+
+app.get('/', (req, res) => {
+  res.send('Yeah my server is running')
+})
+
+
+const pass = "emaWatsonPotter81";
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohnStore").collection("products"); 
+  const ordersCollection = client.db("emaJohnStore").collection("orders");
+
+    app.post('/addProduct', (req, res) => {
+        const products = req.body;
+        productsCollection.insertOne(products)
+        .then(result => {
+            console.log(result.insertedCount); 
+            res.send(result.insertedCount)
+        })
+    })
+
+    // get all product from database
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+        .toArray( (err, documents) => {
+        res.send(documents);
+        })
+    })
+
+    // get single product
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({key: req.params.key})
+        .toArray( (err, documents) =>{
+            res.send(documents[0]);
+        })
+    })
+
+    //post cart key's
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({key: { $in: productKeys}})
+        .toArray( (err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        ordersCollection.insertOne(order)
+        .then(result => {
+            console.log(result.insertedCount); 
+            res.send(result.acknowledged)
+        })
+    })
+});
+
+
+app.listen(port)
